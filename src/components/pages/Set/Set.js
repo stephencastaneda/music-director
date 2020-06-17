1ead7590d710d160e94c75fe55c0675e0b2fc905
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-06-16 20:03:14 -0500

    created components

---
+++ src/components/pages/Set/Set.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Set.scss';
+
+class Set extends React.Component {
+  render() {
+    return (
+      <div className="Set">
+        <h1>Set</h1>
+      </div>
+    );
+  }
+}
+
+export default Set;
---
+++ src/components/pages/Songs/Songs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Songs.scss';
+
+class Songs extends React.Component {
+  render() {
+    return (
+      <div className="Songs">
+        <h1>Songs</h1>
+      </div>
+    );
+  }
+}
+
+export default Songs;
