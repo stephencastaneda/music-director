5958b14a095cb2108ec730307409aa0a3e3ea345
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-06-18 01:03:47 -0500

    delete works

--- src/components/shared/SongList/SongList.js
+++ src/components/shared/SongList/SongList.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 import './SongList.scss';
+
 
 class SongList extends React.Component {
   render() {
-    const { song } = this.props;
+    const { song, removeSong } = this.props;
     const singleLink = `/songs/${song.id}`;
     return (
-      <div className="list-group">
-          <Link href="#" to={singleLink} className="list-group-item list-group-item-action">{song.songTitle} - {song.artist}</Link>
+      <div className="list-container">
+        <ul className="list-group">
+          <li className="list-group-item d-flex justify-content-between align-items-center">
+          <Link to={singleLink} className="list-group-item-action link-tag">{song.songTitle} - {song.artist}</Link>
+          <button className="btn btn-danger song-delete-btn" onClick={() => removeSong(song.id)}><i className="fas fa-trash-alt"></i></button>
+          </li>
+        </ul>
       </div>
     );
   }
